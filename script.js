8dd876e2fc5276e649d3a2768154524f23aaae76
ShivaniF2000 <119884719+ShivaniF2000@users.noreply.github.com>
2022-12-30 23:41:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function success(){
+    // Check mandatory fields
+    let fName=document.getElementById('First-Name').value;
+    let lName=document.getElementById('Last-Name').value;
+    let email=document.getElementById('Email-Address').value;
+    console.log(fName, lName, email);
+ 
+    // show the alert only if all mandatory input fields are filled
+    if(fName&& lName && email){
+        alert("Form Submitted Successfully!!");
+    }
+    else{
+        alert("Please fill the mandaotory fields");
+    }
+   
+}
+}
